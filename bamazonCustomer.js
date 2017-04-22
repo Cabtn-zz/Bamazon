7c8305c6ee8972943e62b1e28e600701dd9ae47f
Abtin Ravanipour <Abtin Ravanipour>
2017-04-22 14:40:49 -0700

    Cleaned up the file and added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,7 @@
 var mysql = require('mysql');
 var prompt = require('prompt');
+var prompts = require('./prompts.js');
 require('console.table');
-
-//This should be an FS can't require a CSV
-// var products = require('./bamazon.csv');
 
 var connection = mysql.createConnection({
     host: "127.0.0.1",
@@ -12,12 +10,12 @@ var connection = mysql.createConnection({
     password: "password",
     database: "bamazon",
 });
-
+//Upon connecting to our server we fetch the products
 connection.connect(function(err,res){
     console.log("connected");
     fetchProducts();
 });
-
+//Selects all of the products from the table and loops through them displaying a table
 const fetchProducts = () => {
     connection.query("SELECT * FROM products", function(err, res) {
             for (var i = 0; i < res.length; i++) {
@@ -26,61 +24,10 @@ const fetchProducts = () => {
         promptSelect();
     })
 };
-
-const productChosen = (result) => {
-    connection.query("SELECT * FROM products WHERE ?", {
-        item_id: result
-    },function (err, res){
-        console.log(res[0].product_name);
-    });
-};
-//SOMETHING IS WRONG WITH THIS FUNCTION;
-const selectQuantity = (x) => {
-    prompt.start();
-    prompt.get(quantityPrompt, function(err,result){
-        if(err) {console.log("error here")}
-        console.log("resss", typeof parseInt(result.quantity));
-        // console.log(result.quantity);
-        connection.query("SELECT * FROM products WHERE ?", {
-            //FIX THIS LINE
-            item_id: x
-        },function(err, res){
-            //PARSE INT STUFF 
-            console.log("LINE 45===" + res[0].stock_quantity);
-                if (res[0].stock_quantity > 0){
-                    console.log("You can have");
-                }
-                else {
-                    console.log("All out");
-                }
-        })
-    })
-};
-
-var itemPrompt = {
-        properties: {
-            item_id: { 
-                message: "Using the item_id, select an product you'd like",
-                warning: 'Must select an ID of 1-8',
-                validator: /^[0-8]*$/,
-                name: "item_id",
-            },
-        }
-};
-
-var quantityPrompt = {
-        properties: {
-            quantity: {
-                message: "How many would you like?",
-                validator: /^[0-9]*$/,
-                name: "stock",
-            },
-        }
-}
-
+//This is asks the user to select an item then determine the quantity they want if they select a valid item_id
 const promptSelect = () => {
     prompt.start();
-    prompt.get(itemPrompt, function(err, result){
+    prompt.get(prompts.itemPrompt, function(err, result){
         console.log(result.item_id);
         if (result.item_id < 9) {
             var x = result.item_id;
@@ -92,5 +39,36 @@ const promptSelect = () => {
             promptSelect();
         }
     });
+};
+//We pass in the results from our promptselect function here and display the name of that function
+const productChosen = (result) => {
+    connection.query("SELECT * FROM products WHERE ?", {
+        item_id: result
+    },function (err, res){
+        console.log(res[0].product_name);
+    });
+};
+//Using the results from promoptSelect we subtract the answer from the stock_quantity if the value is greater than 0. There is a bug here where a user could
+//an excess of stock
+const selectQuantity = (x) => {
+    prompt.start();
+    prompt.get(prompts.quantityPrompt, function(err,result){
+        connection.query("SELECT * FROM products WHERE ?", {
+            item_id: x
+        },function(err, res){
+                if (res[0].stock_quantity > 0){
+                   connection.query("UPDATE products SET ? WHERE ?", [{
+                       stock_quantity: parseInt(res[0].stock_quantity) - parseInt(result.quantity)},
+                       {item_id: x                       
+                    }], function (err, res) {
+                });
+                    console.log("We have successfully fulfilled you're order");
+                }
+                else {
+                    console.log("I'm sorry we don't have enough of that item to fulfill your order");
+                    promptSelect();
+                }
+        })
+    })
 };
 
---
+++ prompts.js
@@ -0,0 +1,25 @@
+var itemPrompt = {
+        properties: {
+            item_id: { 
+                message: "Using the item_id, select an product you'd like",
+                warning: 'Must select an ID of 1-8',
+                validator: /^[0-8]*$/,
+                name: "item_id",
+            },
+        }
+};
+
+var quantityPrompt = {
+        properties: {
+            quantity: {
+                message: "How many would you like?",
+                validator: /^[0-9]*$/,
+                name: "stock",
+            },
+        }
+};
+
+exports.itemPrompt = itemPrompt;
+exports.quantityPrompt = quantityPrompt;
+
+console.log("i'm in here")
